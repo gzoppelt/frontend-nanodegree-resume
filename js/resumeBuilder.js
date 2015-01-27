fde6701942cac28dc16c258ffb9356c7b8999130
Guenther <gzoppelt@gmail.com>
2015-01-27 18:48:44 +0000

    getting it ready for submit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,4 @@
+"use strict";
 var bio = {
     "name": "Günther Zoppelt",
     "role": "Web Developer",
@@ -8,34 +9,33 @@ var bio = {
         "twitter": "You do not catch me there!",
         "location": "Nash Mills, Hertfordshire, U.K."
     },
-    "welcomeMessage": "Welcome to all my friends!",
+    "welcomeMessage": "Freelancing offers always welcome!",
     "skills": [
         "AS/400, IBM iSeries, IBM BD/2",
         "Delphi",
-        "PHP",
-        "Zend-Framework",
-        "Laravel",
-        "JavaScript",
-        "AngularJS",
+        "PHP, Zend-Framework, Laravel",
+        "JavaScript, AngularJS",
         "Front-End-Web-Development (soon to come)"
     ],
     "biopic": "images/Guenther.jpg"
 };
 bio.display = function () {
-    for(var key in bio.contacts) {
+    var head, key, i;
+    head = $('#header');
+    for (key in bio.contacts) {
         $('#topContacts').append(
             HTMLcontactGeneric
                 .replace('%contact%', key)
                 .replace('%data%', bio.contacts[key])
         );
     }
-    $('#header').prepend(HTMLbioPic.replace('%data%', bio.biopic));
-    $('#header').prepend(HTMLheaderRole.replace('%data%', bio.role));
-    $('#header').prepend(HTMLheaderName.replace('%data%', bio.name));
-
-    $("#header").append(HTMLskillsStart);
-    $("#header").append(HTMLskills.replace('%data%', bio.skills[0]));
-    for(var i = 1; i < bio.skills.length; i++){
+    head.prepend(HTMLbioPic.replace('%data%', bio.biopic));
+    head.prepend(HTMLheaderRole.replace('%data%', bio.role));
+    head.prepend(HTMLheaderName.replace('%data%', bio.name));
+    head.append(HTMLWelcomeMsg.replace('%data%', bio.welcomeMessage));
+    head.append(HTMLskillsStart);
+    head.append(HTMLskills.replace('%data%', bio.skills[0]));
+    for(i = 1; i < bio.skills.length; i++){
         $(".flex-item:last").append(HTMLskills.replace('%data%', bio.skills[i]));
     }
 };
@@ -43,19 +43,22 @@ bio.display();
 
 var work = {
     "jobs": [
-        {   "employer": "Autohaus Berndt GmbH & Co. KG",
+        {
+            "employer": "Autohaus Berndt GmbH & Co. KG",
             "title": "IT Administrator",
             "location": "Dresden, Germany",
             "dates": "1994-2002",
             "description": "Maintaining IT infrastructure and minor programming tasks"
         },
-        {   "employer": "Autohaus Schmolck GmbH & Co. KG",
+        {
+            "employer": "Autohaus Schmolck GmbH & Co. KG",
             "title": "IT Administrator",
             "location": "Emmendingen, Germany",
             "dates": "2003-2015",
             "description": "It started with maintaining the IT infrastructure and changed later into programming interfaces between different software solutions."
         },
-        {   "employer": "Z-Bit Solutions Limited",
+        {
+            "employer": "Z-Bit Solutions Limited",
             "title": "Programmer",
             "location": "London, U.K.",
             "dates": "since 2013",
@@ -64,15 +67,17 @@ var work = {
     ]
 };
 work.display = function () {
-    for (var i in work.jobs){
+    var job, we;
+    for (job in work.jobs) {
         $("#workExperience").append(HTMLworkStart);
-        $(".work-entry:last").append(
-            HTMLworkEmployer.replace('%data%', work.jobs[i].employer)
-            +  HTMLworkTitle.replace('%data%', work.jobs[i].title)
+        we = $(".work-entry:last");
+        we.append(
+            HTMLworkEmployer.replace('%data%', work.jobs[job].employer)
+            + HTMLworkTitle.replace('%data%', work.jobs[job].title)
         );
-        $(".work-entry:last").append(HTMLworkLocation.replace('%data%', work.jobs[i].location));
-        $(".work-entry:last").append(HTMLworkDates.replace('%data%', work.jobs[i].dates));
-        $(".work-entry:last").append(HTMLworkDescription.replace('%data%', work.jobs[i].description));
+        we.append(HTMLworkLocation.replace('%data%', work.jobs[job].location));
+        we.append(HTMLworkDates.replace('%data%', work.jobs[job].dates));
+        we.append(HTMLworkDescription.replace('%data%', work.jobs[job].description));
     }
 };
 work.display();
@@ -94,20 +99,21 @@ var projects = {
             "description": "We learned the basics of JavaScript and how to separate content from layout.",
             "images": [
                 "images/Project2_1.png"
-
             ]
         }
     ]
 };
 projects.display = function () {
-    for (var i in projects.projects) {
-        var pro = projects.projects[i];
+    var i, j, pro, pe;
+    for (i in projects.projects) {
+        pro = projects.projects[i];
         $("#projects").append(HTMLprojectStart);
-        $(".project-entry:last").append(HTMLprojectTitle.replace('%data%', pro.title));
-        $(".project-entry:last").append(HTMLprojectDates.replace('%data%', pro.dates));
-        $(".project-entry:last").append(HTMLprojectDescription.replace('%data%', pro.description));
-        for (var j in pro.images) {
-            $(".project-entry:last").append(HTMLprojectImage.replace('%data%', pro.images[j]));
+        pe = $(".project-entry:last");
+        pe.append(HTMLprojectTitle.replace('%data%', pro.title));
+        pe.append(HTMLprojectDates.replace('%data%', pro.dates));
+        pe.append(HTMLprojectDescription.replace('%data%', pro.description));
+        for (j in pro.images) {
+            pe.append(HTMLprojectImage.replace('%data%', pro.images[j]));
         }
     }
 };
@@ -123,7 +129,7 @@ var education = {
                 "Mathematics",
                 "Mathematical Cybernetics and Computing"
             ],
-            "dates": "1979",
+            "dates": 1979,
             "url": "http://tu-dresden.de/en"
         },
         {
@@ -141,39 +147,40 @@ var education = {
         {
             "title": "Nanodegree Web Developer",
             "school": "Udacity",
-            "date": "2015",
+            "date": 2015,
             "url": "https://www.udacity.com/course/nd001"
         }
     ]
 };
 education.display = function () {
+    var edu, uni, i, course;
     $('#education').append(HTMLschoolStart);
-    for (var i in education.schools) {
-        $('.education-entry:last').append(
+    edu = $('.education-entry:last');
+    for (uni in education.schools) {
+        edu.append(
             HTMLschoolName
-                .replace('%data%', education.schools[i].name)
-                .replace('#', education.schools[i].url)
-            + HTMLschoolDegree.replace('%data%', education.schools[i].degree)
+                .replace('%data%', education.schools[uni].name)
+                .replace('#', education.schools[uni].url)
+            + HTMLschoolDegree.replace('%data%', education.schools[uni].degree)
         );
-        $('.education-entry:last').append(HTMLschoolLocation.replace('%data%', education.schools[i].location));
-        $('.education-entry:last').append(HTMLschoolDates.replace('%data%', education.schools[i].dates));
-        for (var j in education.schools[i].majors){
-            $('.education-entry:last').append(HTMLschoolMajor.replace('%data%', education.schools[i].majors[j]));
+        edu.append(HTMLschoolLocation.replace('%data%', education.schools[uni].location));
+        edu.append(HTMLschoolDates.replace('%data%', education.schools[uni].dates));
+        for (i in education.schools[uni].majors) {
+            edu.append(HTMLschoolMajor.replace('%data%', education.schools[uni].majors[i]));
         }
-    };
-    $('.education-entry:last').append(HTMLonlineClasses);
-    for (var i in education.onlineCourses) {
-        $('.education-entry:last').append(
-            HTMLonlineTitle.replace('%data%', education.onlineCourses[i].title) +
-            HTMLonlineSchool.replace('%data%', education.onlineCourses[i].school)
+    }
+    edu.append(HTMLonlineClasses);
+    for (course in education.onlineCourses) {
+        edu.append(
+            HTMLonlineTitle.replace('%data%', education.onlineCourses[course].title) +
+            HTMLonlineSchool.replace('%data%', education.onlineCourses[course].school)
         );
-        $('.education-entry:last').append(HTMLonlineDates.replace('%data%', education.onlineCourses[i].date));
-        $('.education-entry:last').append(
+        edu.append(HTMLonlineDates.replace('%data%', education.onlineCourses[course].date));
+        edu.append(
             HTMLonlineURL
-                .replace('%data%', education.onlineCourses[i].url)
-                .replace('#', education.onlineCourses[i].url)
+                .replace('%data%', education.onlineCourses[course].url)
+                .replace('#', education.onlineCourses[course].url)
         );
     }
 };
 education.display();
-
